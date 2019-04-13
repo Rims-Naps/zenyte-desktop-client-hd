bb47f72100f84c159f51083be8b5231028e18c4a
Kris <kris@zenyte.com>
2019-04-13 14:06:37 +0300

    Antidrag plugin resolved.

--- runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/antidrag/AntiDragPlugin.java
@@ -26,7 +26,9 @@ package net.runelite.client.plugins.antidrag;
 
 import com.google.inject.Provides;
 import net.runelite.api.Client;
+import net.runelite.api.events.ConfigChanged;
 import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
@@ -65,5 +67,12 @@ public class AntiDragPlugin extends Plugin
 	{
 		client.setInventoryDragDelay(DEFAULT_DELAY);
 	}
+
+    @Subscribe
+    public void onConfigChanged(ConfigChanged event) {
+        if (event.getGroup().equals("antiDrag")) {
+            client.setInventoryDragDelay(config.dragDelay());
+        }
+    }
 
 }
